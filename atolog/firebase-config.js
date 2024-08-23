85cc6e1e7b991f125b6e8391fc550856e17af839
SAMUEL CHIBUNNA <officialpatoo@icloud.com>
2024-08-23 13:04:15 -0700

    added google firebase configuration
    updates add firebase SDK
    
    Signed-off-by: SAMUEL CHIBUNNA <officialpatoo@icloud.com>

---
+++ atolog/firebase-config.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDjf6PSBi4TJy6uilcpfSEic-_YCzUa38s",
+  authDomain: "elite-flow-386908.firebaseapp.com",
+  databaseURL: "https://elite-flow-386908-default-rtdb.firebaseio.com",
+  projectId: "elite-flow-386908",
+  storageBucket: "elite-flow-386908.appspot.com",
+  messagingSenderId: "81792132824",
+  appId: "1:81792132824:web:e82ab8cd4bc0b47b0299ad",
+  measurementId: "G-LQSZ43LS7D"
+};
